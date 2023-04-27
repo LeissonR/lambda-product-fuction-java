b3318388a945c0f4b6009fb1d61f77e542aeaaaf
Leisson Rodrigues <leisson@gmail.com>
2023-04-27 01:58:10 -0400

    Reference method staticUpperCaseName

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,7 +6,6 @@ import java.util.Locale;
 import java.util.stream.Collectors;
 
 import entities.Product;
-import util.UpperCaseNameFuction;
 
 public class Program {
 
@@ -21,7 +20,9 @@ public class Program {
 		list.add(new Product("Tablet", 350.00));
 		list.add(new Product("HD Case", 80.90));
 
-	List<String> names =  list.stream().map(new UpperCaseNameFuction()).collect(Collectors.toList());
+	//List<String> names =  list.stream().map(new UpperCaseNameFuction()).collect(Collectors.toList());
+	
+	List<String> names =  list.stream().map(Product::staticUpperCaseName).collect(Collectors.toList());
 	
 	names.forEach(System.out::println);
 	}
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -26,12 +26,8 @@ public class Product {
 		this.price = price;
 	}
 
-	public static void staticPriceUpdate(Product p) {
-		p.setPrice(p.getPrice() * 1.10);
-	}
-
-	public void nonStaticPriceUpdate() {
-		setPrice(getPrice() * 1.10);
+	public static String staticUpperCaseName(Product p) {
+		return p.getName().toUpperCase();
 	}
 
 	@Override
